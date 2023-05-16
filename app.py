b3d1c2c732696631b84517abe1834cb86a7e7ba9
NIK98RAJ <nik98.raj@gmail.com>
2023-05-16 23:01:40 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+API_URL = "https://frontend-production-2a34.up.railway.app/api/v1/prediction/39c3cac1-3df6-4b33-8788-500f6b670d52"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form.get('message')
+    payload = {"question": message}
+    response = requests.post(API_URL, json=payload)
+    return jsonify(response.json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
