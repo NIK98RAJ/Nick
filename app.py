7f2c89270bbd4532128a56c47e6a59f58a001782
NIK98RAJ <nik98.raj@gmail.com>
2023-05-16 23:27:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 
+import os
+
 app = Flask(__name__)
 
 API_URL = "https://frontend-production-2a34.up.railway.app/api/v1/prediction/39c3cac1-3df6-4b33-8788-500f6b670d52"
 
 @app.route('/')
 def home():
+    print(os.getcwd())
     return render_template('index.html')
 
 @app.route('/send_message', methods=['POST'])
@@ -14,7 +17,9 @@ def send_message():
     message = request.form.get('message')
     payload = {"question": message}
     response = requests.post(API_URL, json=payload)
-    return jsonify(response.json())
+
+
+    return response.text
 
 if __name__ == "__main__":
     app.run(debug=True)
